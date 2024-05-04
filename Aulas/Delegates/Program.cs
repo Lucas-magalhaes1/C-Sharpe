5ad2453d9604995a5b02bc204fb9bf5e05848aa6
Lucas <lucasmagalhaes728@gmail.com>
2024-05-04 18:10:21 -0300

    New Class

---
+++ Aulas/Delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using Course.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+
+{
+
+    delegate double BinariyNumericOperation(Double n1, double n2);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a = 10;
+            double b = 12;
+
+            BinariyNumericOperation op = CalculationService.Sum;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Aulas/Delegates/Sevice/CalculationService].cs
@@ -0,0 +1,18 @@
+﻿namespace Course.Services
+{
+    class CalculationService
+    {
+        public static double Max(double x, double y)
+        {
+            return (x > y) ? x : y;
+        }
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Square(double x)
+        {
+            return x * x;
+        }
+    }
+}

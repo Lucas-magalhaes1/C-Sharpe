bd2a370933aff7516a2676dfba3f9c6e3ffd34f3
Lucas <lucasmagalhaes728@gmail.com>
2024-05-04 18:17:02 -0300

    New Class

---
+++ Aulas/MulticastDelegate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Course.Services;
+namespace Course
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+            op(a, b);
+        }
+    }
+}

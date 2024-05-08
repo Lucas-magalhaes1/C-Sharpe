f761ca4cea5024db56a4cc0d4c01ff0667c67098
Lucas <lucasmagalhaes728@gmail.com>
2024-05-08 10:19:43 -0300

    New Class

---
+++ Aulas/Func/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Func.Entities
+{
+    internal class Product
+    {
+        public String Name { get; set; }
+        public Double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override String ToString()
+        {
+            return Name + " , " + Price;
+        }
+    }
+}
---
+++ Aulas/Func/Program.cs
@@ -0,0 +1,36 @@
+﻿using Func.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Func
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            Func<Product, String> func = NameUpper;             // expressão lambda  func = p => p.name.ToUpper();
+
+            List<string> result = list.Select(func).ToList();
+            foreach(string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static string NameUpper (Product p)
+        {
+            return p.Name.ToUpper();
+        }
+    }
+}

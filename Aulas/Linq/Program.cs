4928b86112d5e523613c9bd1b2d6e63592697f91
Lucas <lucasmagalhaes728@gmail.com>
2024-05-08 10:44:10 -0300

    New Class

---
+++ Aulas/Linq/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Especificar o data source 
+
+            int[] num = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            // defirnir a consulta
+
+            var result = num
+                .Where(x => x % 2 == 0)
+                .Select(X => X * 10);
+
+            //executar consulta
+
+            foreach(int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}

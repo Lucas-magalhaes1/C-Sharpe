27d8c4ca05503865dcb68ea94a9d5b973fb5861d
Lucas <lucasmagalhaes728@gmail.com>
2024-05-09 10:58:44 -0300

    New Class

---
+++ Aulas/LinqAndLambda/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAndLambda.Entities
+{
+    internal class Category
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+
+    }
+}
---
+++ Aulas/LinqAndLambda/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAndLambda.Entities
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public override string ToString()
+        {
+            return Id + " , " + Name + " , " + Price + Category.Name + " , " + Category.Tier;
+        }
+    }
+}
